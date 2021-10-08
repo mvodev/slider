d5cc70f4754a50d9fa9a4511cf8f432919eb214d
Vadim <vadimmanushin@yandex.ru>
2021-10-08 22:26:14 +0300

    added variables in setColoredRange method of ColoredRange class

--- src/view/components/ColoredRange.ts
+++ src/view/components/ColoredRange.ts
@@ -21,27 +21,31 @@ class ColoredRange {
   ): void {
     if (viewSettings.isRange) {
       if (viewSettings.isVertical) {
+        const height = `${(Number.parseInt(thumbTo.style.top, 10)
+            - Number.parseInt(thumbFrom.style.top, 10) + thumbWidthInPercentage * 0.6)}%`;
         this.coloredRange.style.top = thumbFrom.style.top;
         this.coloredRange.style.left = '0%';
         this.coloredRange.style.width = '100%';
-        this.coloredRange.style.height = `${(Number.parseInt(thumbTo.style.top, 10)
-          - Number.parseInt(thumbFrom.style.top, 10) + thumbWidthInPercentage * 0.6)}%`;
+        this.coloredRange.style.height = height;
       } else {
+        const width = `${(Number.parseInt(thumbTo.style.left, 10)
+          - Number.parseInt(thumbFrom.style.left, 10) + thumbWidthInPercentage * 0.6)}%`;
         this.coloredRange.style.left = thumbFrom.style.left;
         this.coloredRange.style.top = '0%';
         this.coloredRange.style.height = '100%';
-        this.coloredRange.style.width = `${(Number.parseInt(thumbTo.style.left, 10)
-          - Number.parseInt(thumbFrom.style.left, 10) + thumbWidthInPercentage * 0.6)}%`;
+        this.coloredRange.style.width = width;
       }
     } else if (!viewSettings.isRange) {
       if (viewSettings.isVertical) {
+        const height = `${(Number.parseInt(thumbFrom.style.top, 10)
+          + thumbWidthInPercentage * 0.5)}%`;
         this.coloredRange.style.top = '0%';
         this.coloredRange.style.width = '100%';
-        this.coloredRange.style.height = `${(Number.parseInt(thumbFrom.style.top, 10)
-          + thumbWidthInPercentage * 0.5)}%`;
+        this.coloredRange.style.height = height;
       } else {
-        this.coloredRange.style.width = `${(Number.parseInt(thumbFrom.style.left, 10)
+        const width = `${(Number.parseInt(thumbFrom.style.left, 10)
           + thumbWidthInPercentage * 0.5)}%`;
+        this.coloredRange.style.width = width;
         this.coloredRange.style.left = '0%';
         this.coloredRange.style.top = '0%';
         this.coloredRange.style.height = '100%';
